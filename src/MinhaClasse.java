c604bc29de975d35877aa54b1a8b09b469a7a605
AdeSilva <adesilva.dev@gmail.com>
2023-08-29 00:09:33 -0300

    Aula de Java - Anatomias 1,2 e 3.

---
+++ src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+       String meuNome = "Ademilton";
+       int anoNascimento = 1980;
+       boolean souAlto = true;
+       double minhaAltura = 1.92;
+
+        System.out.println("Me chamo "+ meuNome);
+        System.out.println("Nasci em "+anoNascimento);
+        System.out.println("Eu tenho "+ minhaAltura + " de altura.");
+
+    }
+}
---
+++ src/MinhaClasse2.java
@@ -0,0 +1,18 @@
+public class MinhaClasse2 {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Ademilton";
+        String segundoNome = "da Silva";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        return "Eu me chamo " + primeiroNome + " " + segundoNome;
+
+    }
+}
